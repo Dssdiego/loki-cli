45e262b6ee9df6bb9a1de219e55863db0d27bdb9
Diego Santos Seabra <dss-diego@hotmail.com>
2020-02-03 18:56:48 -0300

    [WIP] Hex 2 Str

--- bin/loki.js
+++ bin/loki.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+'use strict'
+
 const prog = require('caporal');
 
 const commands = require('../lib/commands.js');
@@ -6,5 +9,10 @@ prog
     .version('1.0.0')
     .command('about', 'About the app')
     .action(commands.about);
+
+prog
+    .command('', 'Converts HEX to String')
+    .argument('[hex...]', 'HEX to be converted')
+    .action(commands.hex2str);
 
 prog.parse(process.argv);
